d272d8b1e39b6c85b7f1b8c0b9d3aab95466d576
joelsonlins <joelsonlins@hotmail.com>
2024-11-20 19:36:31 -0300

    Land page Mario Bros vers. 1.0

---
+++ Modulo03/atividade-06/js/app.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const swiper = new Swiper(".swiper", {
+  
+    direction: "horizontal",
+  
+    loop: false,
+  
+
+    pagination: {
+      el: ".swiper-pagination",
+  
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  
+    },
+  });
+
+  document.addEventListener("keydown", function (event) {
+  
+    if (event.key === "ArrowLeft") {
+      swiper.slidePrev();
+  
+    } else if (event.key === "ArrowRight") {
+      swiper.slideNext();
+  
+    }
+  });
+
+  const gameInput = document.getElementById("gameInput");
+  
+
+  gameInput.addEventListener("keypress", function (event) {
+  
+    if (event.key === "Enter") {
+  
+      let searchGame = gameInput.value.trim().toUpperCase();
+  
+
+      switch (searchGame) {
+        case "MARIO":
+          swiper.slideTo(0);
+  
+          break;
+        case "POKEMON":
+          swiper.slideTo(1);
+  
+          break;
+        case "ZELDA":
+          swiper.slideTo(2);
+  
+          break;
+        default:
+          alert("Jogo não foi encontrado");
+  
+          break;
+      }
+    }
+  });
+});
